7f288a6b1a06155f26f30f47801fee20a5510474
Rahimi0151 <rahimi0151@gmail.com>
2019-01-21 13:17:24 +0330

    refactored some functions to make up to 99% less SQL insert while populating database with dummy data.

--- Main.php
+++ Main.php
@@ -8,7 +8,16 @@
 
     $arash = new insert();
 
-    $arash->ininialize_database_with_fake_data(0.001);
+    $map = [
+        ['table_name' => 'player'   , 'count' => 300],
+        ['table_name' => 'stadium'  , 'count' => 10],
+        ['table_name' => 'team'     , 'count' => 18],
+        ['table_name' => 'contract' , 'count' => 290],
+        ['table_name' => 'referee'  , 'count' => 10],
+        ['table_name' => 'match'    , 'count' => 250]
+    ];
+
+    $arash->ininialize_database_with_fake_data($map);
 
     //$arash->remove_all_data_from_table_player();
 
--- insert.php
+++ insert.php
@@ -11,12 +11,25 @@
             $this->db = database_connector::new_database_connection();
         }
 
+        public function remove_quotation_mark_of($string){
+            return str_replace( "'" , "" , $string );
+        }
+
         public function find_how_many_rows_was_already_inserted_in($table){
             $query = "
                 SELECT COUNT(*) FROM \"$table\" "
             ;
             foreach ($this->db->query($query) as $row){
                 return $row['count'];
+            }
+        }
+
+        public function find_last_inserted_id_in($table){
+            $query = "
+                SELECT MAX(id) FROM \"$table\" "
+            ;
+            foreach ($this->db->query($query) as $row){
+                return $row['max'];
             }
         }
 
@@ -31,158 +44,179 @@
         /*
             creating query string SECTION
         */
-        
-        public function create_query_string_for_inserting_into_stadium(){
 
-            $id = $this->find_how_many_rows_was_already_inserted_in('stadium') + 1;
-            $name = $this->faker->lastName;
-            $capacity = rand( 1 , 10 ) * 10000 ;
-            $age = rand( 0 , 100 );
+        public function create_this_many_query_string_for_inserting_into_stadium($how_many){
+
+            $id = $this->find_how_many_rows_was_already_inserted_in('stadium');
+            $query = '';
+
+            for ( $i = 0 ; $i < $how_many ; $i++ ){
+                $id++;
+                $name = $this->remove_quotation_mark_of($this->faker->lastName);
+                $capacity = rand( 1 , 10 ) * 10000 ;
+                $age = rand( 0 , 100 );
             
-            $query = "
-                INSERT INTO \"stadium\" ( \"id\" , \"name\" , \"capacity\" , \"age\" )
-                VALUES ( $id , '$name' , $capacity , $age ); 
-            ";
-
+                $query .= "
+                    INSERT INTO \"stadium\" ( \"id\" , \"name\" , \"capacity\" , \"age\" )
+                    VALUES ( $id , '$name' , $capacity , $age ); 
+                ";
+            }
             return $query;
         }
 
-        public function create_query_string_for_inserting_into_player(){
+        public function create_this_many_query_string_for_inserting_into_player($how_many){
 
-            $id = $this->find_how_many_rows_was_already_inserted_in('player') + 1;
-            $first_name = $this->faker->firstName('male');
-            $last_name = $this->faker->lastName;
-            $price = rand( 1 , 10 ) * 10000 ;
-            $age = rand( 16 , 40 );
+            $id = $this->find_last_inserted_id_in('player');
+            $query = '';
+
+            for ( $i = 0 ; $i < $how_many ; $i++ ){
+                $id++;
+                $first_name = $this->remove_quotation_mark_of($this->faker->firstName('male'));
+                $last_name = $this->remove_quotation_mark_of($this->faker->lastName);
+                $price = rand( 1 , 10 ) * 10000 ;
+                $age = rand( 16 , 40 );
+                
+                $query .= "
+                    INSERT INTO \"player\" ( \"id\" , \"first-name\" , \"last-name\" , \"age\" , \"price\" )
+                    VALUES ( $id , '$first_name' , '$last_name' , $age , $price ); 
+                ";
+            }
+            return $query;
+        }
+
+        public function create_this_many_query_string_for_inserting_into_team($how_many){
+
+            $id = $this->find_last_inserted_id_in('team');
+            $stadium_count = $this->find_how_many_rows_was_already_inserted_in('stadium');
+            $query = '';
+
+            for ( $i = 0 ; $i < $how_many ; $i++ ){
+                $id++;
+                $name = $this->remove_quotation_mark_of( $this->faker->lastName );
+                $coach = $this->remove_quotation_mark_of( $this->faker->name );
+                $captain = $this->remove_quotation_mark_of( $this->faker->name );
+                $main_stadium = rand( 1 , $stadium_count );
+                $budget = rand( 15 , 100 ) * 1000 * 1000 ;
+                
+                $query .= "
+                    INSERT INTO \"team\" ( \"id\" , \"name\" , \"coach\" ,
+                     \"captain\" , \"main-stadium\" , \"budget\" )
+                    VALUES ( $id , '$name' , '$coach' , '$captain' , $main_stadium , $budget ); 
+                ";
+            }
+            return $query;
+        }
+
+        public function create_this_many_query_string_for_inserting_into_contract($how_many){
+
+            $team_count = $this->find_how_many_rows_was_already_inserted_in('team');
+
+            $id = $this->find_how_many_rows_was_already_inserted_in('contract');
+            $player_id = $this->find_how_many_rows_was_already_inserted_in('contract');     
+            $query = '';
             
-            $query = "
-                INSERT INTO \"player\" ( \"id\" , \"first-name\" , \"last-name\" , \"age\" , \"price\" )
-                VALUES ( $id , '$first_name' , '$last_name' , $age , $price ); 
-            ";
+            for ( $i = 0 ; $i < $how_many ; $i++ ){
+                $id++;
+                $player_id++;
+                $team_id = rand( 1 , $team_count );
+                $expiration_date = rand( 2019 , 2030 );
+                
+                $query .= "
+                    INSERT INTO \"contract\" ( \"id\" , \"player-id\" , \"team-id\" , \"expiration-date\" )
+                    VALUES ( $id , $player_id , $team_id , $expiration_date ); 
+                ";
+            }
 
             return $query;
         }
 
-        public function create_query_string_for_inserting_into_team(){
+        public function create_this_many_query_string_for_inserting_into_referee($how_many){
 
-            $id = $this->find_how_many_rows_was_already_inserted_in('team') + 1;
-            $name = $this->faker->lastName;
-            $coach = $this->faker->name;
-            $captain = $this->faker->name;
-            $main_stadium = rand( 1 , $this->find_how_many_rows_was_already_inserted_in('stadium') );
-            $budget = rand( 15 , 100 ) * 1000 * 1000 ;
-            
-            $query = "
-                INSERT INTO \"team\" ( \"id\" , \"name\" , \"coach\" ,
-                 \"captain\" , \"main-stadium\" , \"budget\" )
-                VALUES ( $id , '$name' , '$coach' , '$captain' , $main_stadium , $budget ); 
-            ";
+            $id = $this->find_how_many_rows_was_already_inserted_in('referee');
+            $query = '';
 
+            for ( $i = 0 ; $i < $how_many ; $i++ ){
+                $id++;
+                $first_name = $this->remove_quotation_mark_of($this->faker->firstName('male'));
+                $last_name = $this->remove_quotation_mark_of($this->faker->lastName);
+                $age = rand( 23 , 50 );
+
+                $query .= "
+                    INSERT INTO \"referee\" ( \"id\" , \"first-name\" , \"last-name\" , \"age\" )
+                    VALUES ( $id , '$first_name' , '$last_name' , $age ); 
+                ";
+            }
             return $query;
         }
 
-        public function create_query_string_for_inserting_into_contract(){
+        public function create_this_many_query_string_for_inserting_into_match($how_many){
 
-            $id = $this->find_how_many_rows_was_already_inserted_in('contract') + 1;
-            $player_id = $this->find_how_many_rows_was_already_inserted_in('contract') + 1;
-            $team_id = rand( 1 , $this->find_how_many_rows_was_already_inserted_in('team') );
-            $expiration_date = rand( 2019 , 2030 );
-            $query = "
-                INSERT INTO \"contract\" ( \"id\" , \"player-id\" , \"team-id\" , \"expiration-date\" )
-                VALUES ( $id , $player_id , $team_id , $expiration_date ); 
-            ";
+            $id = $this->find_how_many_rows_was_already_inserted_in('match');
+            $team_count = $this->find_how_many_rows_was_already_inserted_in('team');
+            $referee_count = $this->find_how_many_rows_was_already_inserted_in('referee');
+            $stadium_count = $this->find_how_many_rows_was_already_inserted_in('stadium');
+            $query = '';
 
+            for ( $i = 0 ; $i < $how_many ; $i++ ){
+                $id++;
+                $home_id = rand( 1 , $team_count );
+                $away_id = rand( 1 , $team_count );
+                $referee_id = rand( 1 , $referee_count );
+                $stadium_id = rand( 1 , $stadium_count );
+                $home_goals = rand( 0 , 5 );
+                $away_goals = rand( 0 , 5 );
+                $total_attendance = rand( 0 , 100*1000 );
+                
+                $query .= "
+                    INSERT INTO \"match\" ( \"id\" , \"home-id\" ,
+                     \"away-id\" , \"referee-id\" , \"stadium-id\" ,
+                      \"home-goals\" , \"away-goals\", \"total-attendance\" )
+                    VALUES ( $id , '$home_id' , '$away_id' , $referee_id ,
+                    $stadium_id , $home_goals , $away_goals , $total_attendance ); 
+                ";
+            }
             return $query;
         }
 
-        public function create_query_string_for_inserting_into_referee(){
+        /*
+            END OF creating query string SECTION
+        */
 
-            $id = $this->find_how_many_rows_was_already_inserted_in('referee') + 1;
-            $first_name = $this->faker->firstName('male');
-            $last_name = $this->faker->lastName;
-            $age = rand( 23 , 50 );
-            $query = "
-                INSERT INTO \"referee\" ( \"id\" , \"first-name\" , \"last-name\" , \"age\" )
-                VALUES ( $id , '$first_name' , '$last_name' , $age ); 
-            ";
-
-            return $query;
-        }
-
-        public function create_query_string_for_inserting_into_match(){
-
-            $id = $this->find_how_many_rows_was_already_inserted_in('match') + 1;
-            $home_id = rand( 0 , $this->find_how_many_rows_was_already_inserted_in('team') );
-            $away_id = rand( 0 , $this->find_how_many_rows_was_already_inserted_in('team') );
-            $referee_id = rand( 0 , $this->find_how_many_rows_was_already_inserted_in('referee') );
-            $stadium_id = rand( 0 , $this->find_how_many_rows_was_already_inserted_in('stadium') );
-            $home_goals = rand( 0 , 5 );
-            $away_goals = rand( 0 , 5 );
-            $total_attendance = rand( 0 , 100*1000 );
-            
-            $query = "
-                INSERT INTO \"match\" ( \"id\" , \"home-id\" ,
-                 \"away-id\" , \"referee-id\" , \"stadium-id\" ,
-                  \"home-goals\" , \"away-goals\", \"total-attendance\" )
-                VALUES ( $id , '$home_id' , '$away_id' , $referee_id ,
-                $stadium_id , $home_goals , $away_goals , $total_attendance ); 
-            ";
-
-            return $query;
-        }
-
-        public function create_query_string_for_inserting_into($table){{
+        public function create_query_string_for_inserting_into($table , $how_many){
             switch ($table) {
 
                 case 'stadium':
-                    return $this->create_query_string_for_inserting_into_stadium();
+                    return $this->create_this_many_query_string_for_inserting_into_stadium($how_many);
 
                 case 'player':
-                    return $this->create_query_string_for_inserting_into_player();
+                    return $this->create_this_many_query_string_for_inserting_into_player($how_many);
 
                 case 'team':
-                    return $this->create_query_string_for_inserting_into_team();
+                    return $this->create_this_many_query_string_for_inserting_into_team($how_many);
                 
                 case 'contract':
-                    return $this->create_query_string_for_inserting_into_contract();
+                    return $this->create_this_many_query_string_for_inserting_into_contract($how_many);
 
                 case 'referee':
-                    return $this->create_query_string_for_inserting_into_referee();
+                    return $this->create_this_many_query_string_for_inserting_into_referee($how_many);
 
                 case 'match':
-                    return $this->create_query_string_for_inserting_into_match();
+                    return $this->create_this_many_query_string_for_inserting_into_match($how_many);
 
                 default:
                     return null;
             }
-        }}
-
-        public function insert_fake_data_into( $table , $how_many ){
-            for( $i = 0 ; $i < $how_many ; $i++ ){
-                $query = $this->create_query_string_for_inserting_into($table);
-                echo $query;
-                try{
-                    $this->db->exec($query);
-                }
-                catch(exception $e){
-                    echo "$e </hr>";
-                }
-            }
         }
 
-        public function ininialize_database_with_fake_data($timeout){
-            
-            $this->insert_fake_data_into( 'player' , 10 );
-            sleep($timeout);
-            $this->insert_fake_data_into( 'stadium' , 10 );
-            sleep($timeout);
-            $this->insert_fake_data_into( 'team' , 10 );
-            sleep($timeout);
-            $this->insert_fake_data_into( 'contract' , 10 );
-            sleep($timeout);
-            $this->insert_fake_data_into( 'referee' , 10 );
-            sleep($timeout);
-            $this->insert_fake_data_into( 'match' , 10 );
+        public function insert_fake_data_into( $table , $how_many ){
+            $query = $this->create_query_string_for_inserting_into($table , $how_many);
+            echo $query;
+            $this->db->exec($query);
+        }
+
+        public function ininialize_database_with_fake_data($map){
+            foreach ( $map as $table )
+                $this->insert_fake_data_into( $table['table_name'] , $table['count'] );
         }
 
     }
